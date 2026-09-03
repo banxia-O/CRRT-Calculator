c63eaa23078bde6dd487bbc28c93e28c3dc1c87c
LIN <licaixia529@gmail.com>
2026-09-03 12:34:40 +0800

    build: inline app into one offline html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [react(), viteSingleFile()],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100_000_000,
+  },
 })
